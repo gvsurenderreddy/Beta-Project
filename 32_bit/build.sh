a173ba24f9fab4e270069b3fef5244e727b61b32
Marcin Borowicz <porucznik13@gmail.com>
2016-05-18 10:51:44 +0200

    Oddzielenie wersji 32 i 64 bit, wyłączenie wsparcia da UEFI, dodanie czystego obrazu multiarch do testów, ogólne porządki

--- 32_bit/build.sh
+++ 32_bit/build.sh
@@ -249,19 +249,17 @@ mkdir -p ${work_dir}
 run_once make_pacman_conf
 
 # Do all stuff for each airootfs
-for arch in i686 x86_64; do
+for arch in i686; do
     run_once make_basefs
     run_once make_packages
 done
 
-run_once make_packages_efi
-
-for arch in i686 x86_64; do
+for arch in i686; do
     run_once make_setup_mkinitcpio
     run_once make_customize_airootfs
 done
 
-for arch in i686 x86_64; do
+for arch in i686; do
     run_once make_boot
 done
 
@@ -269,10 +267,9 @@ done
 run_once make_boot_extra
 run_once make_syslinux
 run_once make_isolinux
-run_once make_efi
-run_once make_efiboot
 
-for arch in i686 x86_64; do
+
+for arch in i686; do
     run_once make_prepare
 done
 
